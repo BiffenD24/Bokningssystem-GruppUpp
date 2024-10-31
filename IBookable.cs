b6419723e640e3c895018873f0e2aca1d6340260
Hannes Davik <Hannes.davik@gmail.com>
2024-10-31 13:26:41 +0100

    Added methods in Interface

--- IBookable.cs
+++ IBookable.cs
@@ -8,6 +8,12 @@ namespace Bokningssystem_GruppUpp
 {
     internal interface IBookable
     {
+        void NewBookable();
+        void PrintBookings();
+        void UpdateBooking();
+        void RemoveBooking();
+        void SortByYear();
+
 
     }
 }
