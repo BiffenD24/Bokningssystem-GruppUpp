b9b5da2a979cabbfc5624b6adfb9ce2cc9294b92
Hannes Davik <Hannes.davik@gmail.com>
2024-10-31 12:29:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bokningssystem_GruppUpp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
