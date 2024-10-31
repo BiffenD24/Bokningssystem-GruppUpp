9d41a218466ef29a00354e8784a250cc4628ade2
Dennis Bystrom <dennis.bystrom0407@gmail.com>
2024-10-31 13:12:36 +0100

    Lade till klasser för bokning

---
+++ Local.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bokningssystem_GruppUpp
+{
+    internal class Local
+    {
+        // gemensamma metoder och egenskaper. 1:Namn på rummet, Storlek, Maxtid för bokning
+    }
+}
