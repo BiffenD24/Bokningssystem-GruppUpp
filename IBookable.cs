cfa0460b73d1a030cdd4e53bf3f582563bd2c7a4
Hannes Davik <Hannes.davik@gmail.com>
2024-10-31 12:54:17 +0100

    Added Interface named IBookable

---
+++ IBookable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bokningssystem_GruppUpp
+{
+    internal interface IBookable
+    {
+
+    }
+}
