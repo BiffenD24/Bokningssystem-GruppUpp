51ee5df753d8375a708e418804a9b8f4c58d0d1d
Mohamed <mabdi1456@outlook.com>
2024-11-05 13:29:51 +0100

    Två klasser med Uppdatering

---
+++ Booking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bokningssystem_GruppUpp
+{
+    internal class Booking
+    {
+        public int BookingId { get; set; }
+        public string RoomName { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string BookerName { get; set; }
+
+        // Method to display booking details
+        public override string ToString()
+        {
+            return $"BookingId: {BookingId}, Room: {RoomName}, Time: {StartTime:yyyy-MM-dd HH:mm} - {EndTime:HH:mm}, Booker: {BookerName}";
+        }
+    }
+}
